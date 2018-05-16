b67bc1b1bbb0c5cbddad183b25443ac18bde77d3
Joshua Rodriguez <jrodriguez@global-aero.com>
2018-05-16 14:51:06 -0400

    Enter

--- first_test.js
+++ first_test.js
@@ -7,4 +7,6 @@ var driver = new webdriver.Builder().
 driver.get('http://testingportal.na-onbaseappsys.global.local/DTM/US/UAS/Home');
 //driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
 driver.findElement(webdriver.By.name('Get a free quote')).click();
-driver.findElement(webdriver.By.name('Get a free quote')).sendKeys(Keys.ENTER);
+//driver.findElement()
+
+//driver.quit();
