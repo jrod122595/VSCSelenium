3754ef609092ca8449e109a221d5c772af528c28
Joshua Rodriguez <jrodriguez@global-aero.com>
2018-05-16 14:36:41 -0400

    enter

---
+++ first_test.js
@@ -0,0 +1,10 @@
+var webdriver = require('selenium-webdriver');
+ 
+var driver = new webdriver.Builder().
+   withCapabilities(webdriver.Capabilities.chrome()).
+   build();
+ 
+driver.get('http://testingportal.na-onbaseappsys.global.local/DTM/US/UAS/Home');
+//driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
+driver.findElement(webdriver.By.name('Get a free quote')).click();
+driver.findElement(webdriver.By.name('Get a free quote')).sendKeys(Keys.ENTER);
