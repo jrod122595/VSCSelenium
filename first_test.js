61c24fd157d8434791016f600f188cf755b17bfe
Joshua Rodriguez <jrodriguez@global-aero.com>
2018-05-22 10:55:07 -0400

    enter

--- first_test.js
+++ first_test.js
@@ -1,28 +1,37 @@
 var webdriver = require('selenium-webdriver');
- 
+
 //will add compatability for othr browsers in future
 var driver = new webdriver.Builder().
    withCapabilities(webdriver.Capabilities.chrome()).
    build();
 
+
 // links chrome to testing site
 // used to have it go to home but button click messed it up
-driver.get('https://testingportal.ut.aeroinsure.com/DTM/US/UAS/Application');
+driver.get('https://testingportal.ut.aeroinsure.com/DTM/US/UAS/Home');
+
 
 // uses xpath to go to path(next page) based on button click
 //gets to application fields page
 //have to figure out how to automatically pick between indiv or org in different cases
-/*driver.findElement(webdriver.By.xpath("//button[@type='button']")).click().then(function() {
-    drive.sleep(3000);
-}); */
+driver.findElement(webdriver.By.xpath("//button[@type='button']")).click().then(function() {
+    driver.sleep(1000).then(function() {
+        driver.findElement(webdriver.By.xpath("//div[@id='applicantDetails']/div/div/label")).click().then(function() {
+    fields();
+});});});
+
+  
 
 //fills in fields
 //also how to randomly generate fields, probably a func for it
 
-driver.findElement(webdriver.By.xpath("//div[@id='applicantDetails']/div/div/label")).click();
+ 
 
-driver.sleep(2000);
+driver.findElement(webdriver.By.xpath("//div[@id='applicantDetails']/div/div/label")).click().then(function() {
+    fields();
+});
 
+function fields() {
 driver.findElement(webdriver.By.name("firstName")).sendKeys("Joshua");
 driver.findElement(webdriver.By.name("lastName")).sendKeys("Rodriguez");
 driver.findElement(webdriver.By.name("addressLine1")).sendKeys("201 Easton Ave");
@@ -30,25 +39,31 @@ driver.findElement(webdriver.By.name("city")).sendKeys("New Brunswick");
 driver.findElement(webdriver.By.name("zipCode")).sendKeys("07462");
 driver.findElement(webdriver.By.name("email")).sendKeys("jrodriguez@Global-Aero.com");
 driver.findElement(webdriver.By.name("confirmEmail")).sendKeys("jrodriguez@Global-Aero.com");
-driver.findElement(webdriver.By.name("phone")).sendKeys("2016630202");
+driver.findElement(webdriver.By.name("phoneOfApplicant")).sendKeys("2016630202");
+
+driver.wait(
+    webdriver.until.elementLocated(webdriver.By.name("lastInsuranceCarrier")), 2000).then(element => {
+    selectByVisibleText(element, "NONE");
+});
 
 //drop down menu implementation
-//driver.findElement(webdriver.By.name("state")).sendKeys("New Jersey"); sometimes picks NB sometimes NJ
-
 // this driver.wait makes it only pick NJ with selectByVisibletext func
 driver.wait(
     webdriver.until.elementLocated(webdriver.By.name("state")), 2000).then(element => {
-    selectByVisibleText(element, "New Jersey");
+    selectByVisibleText(element, "NEW JERSEY");
 });
 
-// clicks unknown checkbox
-
+//clicks checkbox
 driver.findElement(webdriver.By.xpath("//input[@type='checkbox']")).click();
 
-//driver.findElement(webdriver.By.xpath("//div[@id='applicantForm']/div/form/div/div/div/div/div/ng-form/div/div[6]/div")).sendKeys("robot");
+driver.findElement(webdriver.By.xpath("//div[@id='applicantForm']/div/form/div/div/div/div/div/ng-form/div/div[6]/div")).sendKeys("robot").then(function() {
+    driver.sleep(3000);
+});
 
 
-//driver.sleep(10000);
+driver.findElement(webdriver.By.xpath("//input[@value='Continue']")).click();
+}
+
 
 //driver.findElement(webdriver.By.xpath("//input[@value='Continue']")).click();
 //driver.wait(
@@ -66,8 +81,6 @@ function selectByVisibleText(select, textDesired) {
         });
     });
 }
-
-
 
 function check_title() {
     var promise = driver.getTitle().then(function(title) {
