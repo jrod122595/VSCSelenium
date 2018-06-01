e59b72216e6b66942a0da1f1b3d9d0cce4f80b2c
Joshua Rodriguez <jrodriguez@global-aero.com>
2018-06-01 12:37:50 -0400

    enter

--- premiumTest.js
+++ premiumTest.js
@@ -1,11 +1,11 @@
 var webdriver = require('selenium-webdriver');
-
+var fs = require('fs');
 //will add compatability for other browsers in future
 var driver = new webdriver.Builder().
    withCapabilities(webdriver.Capabilities.chrome()).
    build();
 
-//login page
+   //login page
 driver.get('https://portaluat.global-aero.com/Login');
 
 //log in
@@ -13,10 +13,10 @@ driver.findElement(webdriver.By.id("inputEmail")).sendKeys("jrodriguez");
 driver.findElement(webdriver.By.id("inputPassword")).sendKeys("Test@123");
 
 driver.findElement(webdriver.By.xpath("//input[@value='Login']")).click().then(function() {
-    driver.sleep(1000).then(function() {
+    driver.wait(webdriver.until.elementLocated(webdriver.By.linkText("NEW APPLICATION")), 10000).then(function() {
         driver.findElement(webdriver.By.linkText("NEW APPLICATION")).click().then(function() {
             driver.findElement(webdriver.By.xpath("//div[@id='header']/div/div/div[2]/ul/li[3]/ul/li[2]")).click().then(function() {
-                driver.sleep(2000).then(function() {
+                driver.wait(webdriver.until.elementLocated(webdriver.By.className("aeroinsureSubHeader underlined")), 5000).then(function() {
                     driver.findElement(webdriver.By.xpath("//div[@id='applicantDetails']/div/div/label")).click().then(function() {
                         applicantFields();
                     });
@@ -36,7 +36,8 @@ driver.findElement(webdriver.By.name("email")).sendKeys("jrodriguez@Global-Aero.
 driver.findElement(webdriver.By.name("confirmEmail")).sendKeys("jrodriguez@Global-Aero.com");
 driver.findElement(webdriver.By.name("phoneOfApplicant")).sendKeys("2016630202");
     
-//drop down menu implementation
+    //drop down menu implementation
+    // this driver.wait makes it only pick NJ with selectByVisibletext func
 driver.wait(
     webdriver.until.elementLocated(webdriver.By.name("state")), 10).then(element => {
     selectByVisibleText(element, "NEW JERSEY");
@@ -47,11 +48,8 @@ driver.wait(
     selectByVisibleText(element, "NEW TO MARKET");
 });
 
-//clicks checkbox
-//driver.findElement(webdriver.By.xpath("//input[@type='checkbox']")).click();
-
 driver.findElement(webdriver.By.xpath("//input[@value='Continue']")).click().then(function() {
-    driver.sleep(1000).then(function() {
+    driver.wait(webdriver.until.elementLocated(webdriver.By.id("quoteNumber")), 2000).then(function() {
         operationsFields();
     });
 });
@@ -65,9 +63,8 @@ driver.findElement(webdriver.By.id("operations_operationQuestions_operatedOverPe
 driver.findElement(webdriver.By.id("operations_operationQuestions_publishData-1")).click();
 driver.findElement(webdriver.By.id("operations_operationQuestions_highLiabilityLimit-1")).click();
 driver.findElement(webdriver.By.id("d1d20449-414e-4f1b-86a5-02169db6f5dd")).click();
-
 driver.findElement(webdriver.By.xpath("(//input[@value='Continue'])[2]")).click().then(function() {
-    driver.sleep(1000).then(function() {
+    driver.wait(webdriver.until.elementLocated(webdriver.By.id("quoteNumber")), 2000).then(function() {
         driver.findElement(webdriver.By.xpath("//label[@name='physicalDamageNotRequired']")).click().then(function() {
             droneFields();
         });
@@ -92,7 +89,7 @@ function droneFields() {
     driver.findElement(webdriver.By.name("UASEstimatedAnnualFlightHours")).sendKeys("1000");
     driver.findElement(webdriver.By.name("addUAS")).click();
     driver.findElement(webdriver.By.xpath("(//input[@value='Continue'])[3]")).click().then(function() {
-        driver.sleep(1000).then(function() {
+        driver.wait(webdriver.until.elementLocated(webdriver.By.id("quoteNumber")), 2000).then(function() {
             operatorToClaimFields();
          });
     });
@@ -107,18 +104,25 @@ function operatorToClaimFields() {
     driver.findElement(webdriver.By.xpath("(//input[@value='Continue'])[5]")).click().then(function() {
         driver.sleep(5000).then(function() {
             driver.findElement(webdriver.By.xpath("(//input[@value='Continue'])[6]")).click().then(function() {
-                driver.sleep(1000).then(function() {
+                driver.wait(webdriver.until.elementLocated(webdriver.By.id("quoteNumber")), 5000).then(function() {
                     claimAndliability();
                 });
             });
-         });
-    });
+        });
+    });  
 }
 
 function claimAndliability() {
+    driver.takeScreenshot().then(function(image, error) {
+        fs.writeFile('Premiums.png', image, 'base64', function(error){
+            if (error != null)
+            console.log('error occured while saving screenshot' + error);
+        });
+    }).then(function(){
     driver.findElement(webdriver.By.id("applicantInformation_liability_liabilityPremium_2000000")).click();
     driver.findElement(webdriver.By.id("applicantInformation_liability_personalInjuryLimit_2000000")).click();
     //driver.findElement(webdriver.By.xpath("//input[@value='Submit']")).click();
+    });
 }
 
 //function that searches through drop downs
